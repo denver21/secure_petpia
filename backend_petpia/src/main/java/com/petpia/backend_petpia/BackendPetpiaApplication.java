bda6b69b4dce00ade24c2fc10e96e5e3c00c0325
Yujun Lee <june040929@gmail.com>
2024-04-29 01:11:51 +0900

    first commit

---
+++ backend_petpia/src/main/java/com/petpia/backend_petpia/BackendPetpiaApplication.java
@@ -0,0 +1,13 @@
+package com.petpia.backend_petpia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPetpiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPetpiaApplication.class, args);
+	}
+
+}
---
+++ backend_petpia/src/test/java/com/petpia/backend_petpia/BackendPetpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petpia.backend_petpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPetpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
