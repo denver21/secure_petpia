6a7f1fae4048c019903f58143f7658ce7bbc3f2a
denverahn21@gmail.com <denverahn21@gmail.com>
2024-06-03 01:15:34 +0900

    User Getter/Setting  추가

--- backend_petpia/src/main/java/com/petpia/backend_petpia/entity/User.java
+++ backend_petpia/src/main/java/com/petpia/backend_petpia/entity/User.java
@@ -28,5 +28,60 @@ public class User {
     @Column(name = "Grade")
     private String grade;
 
-    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
 }
