286966d192a9c629135baa5e5a1bda08e8f59e91
denverahn21@gmail.com <denverahn21@gmail.com>
2024-06-04 18:56:40 +0900

    loadUserByUsername 추가

--- backend_petpia/src/main/java/com/petpia/backend_petpia/repository/UserRepository.java
+++ backend_petpia/src/main/java/com/petpia/backend_petpia/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // You can add custom query methods here if needed
+    User findByEmail(String email);
 }
--- backend_petpia/src/main/java/com/petpia/backend_petpia/service/UserService.java
+++ backend_petpia/src/main/java/com/petpia/backend_petpia/service/UserService.java
@@ -3,13 +3,16 @@ package com.petpia.backend_petpia.service;
 import com.petpia.backend_petpia.entity.User;
 import com.petpia.backend_petpia.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -28,5 +31,18 @@ public class UserService {
 
     public void deleteUser(int id) {
         userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER") // you can fetch actual roles/authorities from the user object if needed
+                .build();
     }
 }
